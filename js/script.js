6826a0002a9e3cac446cb9628e83d4fb68809b82
PedroOrtega88 <pedroortegaalonso88@gmail.com>
2023-11-14 00:00:02 +0100

    -a

---
+++ js/script.js
@@ -0,0 +1,72 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const botonPaginaAnterior = document.getElementById('prev-page');
+    const botonPaginaSiguiente = document.getElementById('next-page');
+    const listaPersonajes = document.getElementById('character-list');
+
+    let paginaActual = 1;
+
+    function obtenerPersonajes(pagina) {
+        fetch(`https://rickandmortyapi.com/api/character/?page=${pagina}`)
+            .then((respuesta) => {
+                if (!respuesta.ok) {
+                    throw new Error('La solicitud no fue exitosa');
+                }
+                return respuesta.json();
+            })
+            .then((datos) => {
+                mostrarPersonajes(datos.results);
+            })
+            .catch((error) => {
+                console.error('Error en la solicitud:', error);
+            });
+    }
+
+    function mostrarPersonajes(personajes) {
+        listaPersonajes.innerHTML = '';
+        personajes.forEach((personaje) => {
+            const tarjetaPersonaje = document.createElement('li');
+            tarjetaPersonaje.classList.add('character-card');
+
+            const imagen = document.createElement('img');
+            imagen.src = personaje.image;
+            imagen.alt = personaje.name;
+            imagen.classList.add('character-image');
+
+            const nombre = document.createElement('p');
+            nombre.textContent = personaje.name;
+
+            const especie = document.createElement('p');
+            especie.textContent = personaje.species;
+
+            tarjetaPersonaje.appendChild(imagen);
+            tarjetaPersonaje.appendChild(nombre);
+            tarjetaPersonaje.appendChild(especie);
+
+            listaPersonajes.appendChild(tarjetaPersonaje);
+        });
+    }
+
+    function actualizarPaginacion() {
+        // Actualiza el número de página en la interfaz
+        // Puedes personalizar esto según tu diseño
+        document.getElementById('currentPage').textContent = paginaActual;
+    }
+
+    botonPaginaAnterior.addEventListener('click', () => {
+        if (paginaActual > 1) {
+            paginaActual--;
+            obtenerPersonajes(paginaActual);
+            actualizarPaginacion();
+        }
+    });
+
+    botonPaginaSiguiente.addEventListener('click', () => {
+        paginaActual++;
+        obtenerPersonajes(paginaActual);
+        actualizarPaginacion();
+    });
+
+    // Fetch e inicialización
+    obtenerPersonajes(paginaActual);
+    actualizarPaginacion();
+});
